df807c19fddb7646e9fb8266baa516455fbd4f56
Sagar Chhabriya <129516589+SagarChhabriya@users.noreply.github.com>
2026-02-09 00:12:39 +0500

    Add unit test for multiply function

---
+++ tests/test_math_utils.py
@@ -0,0 +1,9 @@
+from app.math_utils import multiply
+
+def test_mulitply_01():
+  assert multiply(2,3) == 6
+
+
+# First run the workflow with single method then add this one and see how the build process breaks.
+# def test_mulitply_02():
+  # assert multiply(2,3) == 5
